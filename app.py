403c6b14bdff2d107d218aed9360ff74a6f3fccd
Aidan Ho Hayman <aidanho777@gmail.com>
2024-10-26 22:09:51 +0800

    updates

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def chat():
     return jsonify({"response": response})
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5050)))
